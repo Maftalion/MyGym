9bd6e3cfbdfcfd79c219a8ac13996aa80c93bc59
Matt Aftalion <aftalion.matt@gmail.com>
2016-07-11 14:45:04 -0700

    Created Workout Controller

--- Client/app/app.js
+++ Client/app/app.js
@@ -16,4 +16,18 @@ angular.module('myGym', [])
 		});
 	};
 
+	return {
+		fetchAll: fetchAll
+	};
+
+})//Create controller to handle workout requests
+.controller('workoutsCtrl', function($scope, Workouts){
+	//store workouts to scope
+	$scope.data = {};
+
+	//fetch all workouts
+	Workouts.fetchAll().then(function(res, req){
+		$scope.data.workouts = res;
+	});
+
 })
