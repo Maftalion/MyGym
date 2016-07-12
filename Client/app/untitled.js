55346c4c781d5393728ce062ca45b020f862b019
Matt Aftalion <aftalion.matt@gmail.com>
2016-07-12 14:13:09 -0700

    Reformatted JSON data

--- Client/app/untitled.js
+++ Client/app/untitled.js
@@ -5,16 +5,23 @@ fs.readFile('/Users/maftalion/Desktop/2016-06-mvp/Client/app/workouts.json','utf
   if (err) throw err;
   var data = JSON.parse(data);
   var data2 = [];
+  // console.log(data[0])
+  // var data3 = Object.keys(data[0])[0]
+  // console.log(data[0][data3])
   _.each(data, function(item){
   	for (var key in item) {
-  		data2.push({
-  			Excercise: key,
-  			Equipment: item['key'],
-  			Level: key.Level,
-  			Guide: key.guide,
-  			Images: [key.pic_left, key.pic_right]
-  		});
-  	}
+      data2.push({
+       Excercise: key,
+       Equipment: item[key]['Equipment'],
+       Level: item[key]['Level'],
+       Guide: item[key]['guide'],
+       Images: [item[key]['pic_left'], item[key]['pic_right']]
+      });
+    }
   })
-  console.log(data2)
-});
+      
+      // console.log(data2)
+      fs.writeFile('/Users/maftalion/Desktop/2016-06-mvp/Client/app/exercises.json', JSON.stringify(data2), 'utf8')
+})  		
+
+  // console.log()
